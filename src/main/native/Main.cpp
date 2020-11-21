cb5a35917b4691b723ae94391518b322c1bfed80
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-11-20 22:06:01 -0500

    Only use macOS 11 for Actions

--- src/main/native/Main.cpp
+++ src/main/native/Main.cpp
@@ -7,6 +7,7 @@ int main() {
   auto trajectory = frc::TrajectoryGenerator::GenerateTrajectory(
       frc::Pose2d(5_m, 2_m, 0_rad), {}, frc::Pose2d(7_m, 5_m, 90_deg),
       frc::TrajectoryConfig(2_mps, 2_mps_sq));
+
   for (auto state : trajectory.States()) {
     std::cout << state.pose.X() << ", " << state.pose.Y() << std::endl;
   }
